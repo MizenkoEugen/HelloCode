f4504434b93ed9973c44eb891578311a350dd1bd
MizenkoEugen <MizenkoEugen@gmail.com>
2022-10-12 22:47:54 +0300

    add Example005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "евгений")
+{
+    Console.WriteLine("Ура, это же ЖЕНЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
